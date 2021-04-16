54e950bfb5d91a2bf8d86d790fab4fb8493dccf7
WJP2003 <william.powe1996@gmail.com>
2021-04-15 21:16:16 -0500

    Realized the way I implemented caching defeated the entire purpose of having dynamic text components

--- src/main/java/ovh/powenet/carpet_motd_json/Mod.java
+++ src/main/java/ovh/powenet/carpet_motd_json/Mod.java
@@ -20,11 +20,11 @@ public class Mod implements CarpetExtension, ModInitializer, DedicatedServerModI
 
 	private static final Gson gson = new GsonBuilder().setLenient().create();
 
-	public static LiteralText toLegacyText(final String raw) {
+	public static LiteralText toLegacyText(final String raw,boolean[] f) {
 		try {
-			return toLegacyText(resolveTextComponents(gson.fromJson(raw,JsonObject.class)));
+			return toLegacyText(resolveTextComponents(gson.fromJson(raw,JsonObject.class),f));
 		} catch(JsonSyntaxException jsx) {
-			if(raw.startsWith("{") || raw.startsWith("[")) // Checking if it was actually intended to be parsed, or just plaintext
+			if(raw.startsWith("{")) // Checking if it was actually intended to be parsed, or just plaintext
 				return new LiteralText(jsx.toString());
 			else
 				return new LiteralText(raw);
@@ -107,39 +107,44 @@ public class Mod implements CarpetExtension, ModInitializer, DedicatedServerModI
 		b.append(a.get("text").getAsString());
 	}
 
-	private static JsonObject resolveTextComponents(JsonObject a) { return resolveTextComponents(a,true); }
+	private static JsonObject resolveTextComponents(JsonObject a,boolean[] f) { return resolveTextComponents(a,true,f); }
 
-	private static JsonObject resolveTextComponents(JsonObject a,final boolean e) {
+	private static JsonObject resolveTextComponents(JsonObject a,final boolean e,boolean[] f) {
 		MutableText b;
 		// This if-else chain tests for each type of dynamic text type
 		// (except keybinds, since, well, the server doesn't have keybinds)
 		// and creates a ParsableText object accordingly, which is parsed
 		// with Texts.parse().
-		if(a.has("text"))
+		if(a.has("text")) {
 			b = new LiteralText(a.get("text").getAsString());
-		else if(a.has("translate"))
+		} else if(a.has("translate")) {
 			// This line is quite long because it needs to take into account the "with" json array that comes with TranslatableText.
 			// Luckily this isn't that hard, since the JsonArray has an iterator that can
-			b = new TranslatableText(a.remove("translate").getAsString(),a.has("with") && a.get("with").isJsonArray() ? StreamSupport.stream(a.remove("with").getAsJsonArray().spliterator(),false).map(c -> resolveTextComponents(c.getAsJsonObject())).toArray() : null);
-		else if(a.has("score"))
+			b = new TranslatableText(a.remove("translate").getAsString(),a.has("with") && a.get("with").isJsonArray() ? StreamSupport.stream(a.remove("with").getAsJsonArray().spliterator(),false).map(c -> resolveTextComponents(c.getAsJsonObject(),f)).toArray() : null);
+		} else if(a.has("score")) {
 			b = new ScoreText(a.get("score").getAsJsonObject().get("name").getAsString(),a.remove("score").getAsJsonObject().get("objective").getAsString());
-		else if(a.has("selector"))
+			f[0] = true;
+		} else if(a.has("selector")) {
 			b = new SelectorText(a.remove("selector").getAsString());
-		else if(a.has("nbt"))
+			f[0] = true;
+		} else if(a.has("nbt")) {
 			// Block NBT doesn't work because of a weird line in the World.getBlockEntity(pos)
 			// method that checks if the current thread is the server main thread (which in this
 			// case it never is), and if it isn't, it just returns null. Tried to get around
 			// this with a Mixin but it both got overcomplicated and just didn't work. Idk.
                         /*if(a.has("block"))
                                 b = new NbtText.BlockNbtText(a.remove("nbt").toString(),a.has("interpret") && a.remove("interpret").getAsBoolean(),a.remove("block").getAsString());
-                        else*/ if(a.has("entity"))
+                        else*/
+			if(a.has("entity"))
 				b = new NbtText.EntityNbtText(a.remove("nbt").toString(),a.has("interpret") && a.remove("interpret").getAsBoolean(),a.remove("entity").getAsString());
 			else if(a.has("storage"))
 				b = new NbtText.StorageNbtText(a.remove("nbt").toString(),a.has("interpret") && a.remove("interpret").getAsBoolean(),new Identifier(a.remove("storage").getAsString()));
 			else
 				throw new JsonSyntaxException("Invalid nbt tag (Note: block tag not supported)");
-		else
+			f[0] = true;
+		} else {
 			throw new JsonSyntaxException("Invalid text component (No text, translate, score, selector, or nbt tag found)");
+		}
 
 		// This recursively handles extra:[{},{}] tags.
 		// The e flag is there so it doesn't handle extra tags inside of other extra tags (intended behavior).
@@ -150,7 +155,7 @@ public class Mod implements CarpetExtension, ModInitializer, DedicatedServerModI
 					gson.toJsonTree(
 						StreamSupport.stream(
 							q.getAsJsonArray().spliterator(),false
-						).map(c -> resolveTextComponents(c.getAsJsonObject(),false)
+						).map(c -> resolveTextComponents(c.getAsJsonObject(),false,f)
 						).toArray()
 					).getAsJsonArray()
 				);
